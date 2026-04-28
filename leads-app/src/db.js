d346a250b31d3cfcc15de99731e409c8d3f03482
alvares777 <alvares777@hotmail.com>
2026-04-28 16:15:31 -0300

    ALTER TABLE leads ADD COLUMN IF NOT EXISTS whatsapp_jid TEXT;

--- leads-app/src/db.js
+++ leads-app/src/db.js
@@ -4,6 +4,7 @@ const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 // Migrações executadas na inicialização
 pool.query(`
     ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS google_id TEXT;
+    ALTER TABLE leads ADD COLUMN IF NOT EXISTS whatsapp_jid TEXT;
 
     CREATE TABLE IF NOT EXISTS tokens_senha (
         id         SERIAL PRIMARY KEY,
